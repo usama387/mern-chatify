93896dc6b3e82ed521ffa49688ef3c32c0d891ed
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-15 21:15:09 +0500

    added a new api route to get user details by passing token

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, register, updateUserProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, register, updateUserProfile } from "../controllers/auth.controller.js";
 import { verifyUser } from "../middlewares/verifyUser.middleware.js";
 
 const authRouter = express.Router();
@@ -8,6 +8,10 @@ authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 
+// routes with middleware
 authRouter.put("/updateProfile", verifyUser, updateUserProfile);
+
+// to return back user details
+authRouter.get("/check", verifyUser, checkAuth);
 
 export default authRouter;
