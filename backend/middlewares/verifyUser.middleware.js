d46ffa1cdded1e02fc82e14776693feba4201bec
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-15 20:18:02 +0500

    added new api route update profile with middleware to verify user

---
+++ backend/middlewares/verifyUser.middleware.js
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const verifyUser = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Token is required" });
+    }
+
+    // verify the token validity
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Token is invalid" });
+    }
+
+    // find the user by their id in token
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // user is not authenticated
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.error("Error verifying user:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error verifying user" });
+  }
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { login, logout, register, updateUserProfile } from "../controllers/auth.controller.js";
+import { verifyUser } from "../middlewares/verifyUser.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+
+authRouter.put("/updateProfile", verifyUser, updateUserProfile);
 
 export default authRouter;
