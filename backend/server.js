bbde5bf4c19f7ced963435526730eb7d0a25f0bd
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-15 20:17:01 +0500

    added cookie parser support in backend to parse cookies from request headers

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.route.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { connectToDb } from "./lib/mongodb.js";
+import cookieParser from "cookie-parser";
 
 // app config
 const app = express();
 
 // middlewares
 app.use(express.json());
+
+// cookie parser middleware to parse cookies from request headers
+app.use(cookieParser());
+
+// cors middleware to enable cross-origin resource sharing (CORS) from all ips
 app.use(cors());
 
 // dotenv config
 dotenv.config();
-
 
 // test api end point
 app.get("/", (req, res) => {
@@ -21,10 +26,10 @@ app.get("/", (req, res) => {
 });
 
 // auth apis end point
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`The app is running on port ${port}`);
-  connectToDb()
+  connectToDb();
 });
