04be72971f4e1b3acf3324a26b83fe26100bcf71
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-16 10:23:44 +0500

    Added a new model Message using mongoose

---
+++ backend/models/message.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timeStamps: true }
+);
+
+const Message =
+  mongoose.models.Message || mongoose.model("Message", messageSchema);
+
+export default Message;
