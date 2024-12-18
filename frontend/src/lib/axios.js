d1571206af067d2634015f606bde97130490d98c
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-18 14:52:52 +0500

    added axios configuration to make http requests and send cookies to backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const axios = axios.create({
-  baseURL: "http://localhost:4000/api",
+export const axiosInstance = axios.create({
+  baseURL: "http://localhost:4000",
   withCredentials: true, // send cookies with requests
 });
